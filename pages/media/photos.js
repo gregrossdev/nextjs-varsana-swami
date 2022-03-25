6c6c9ae833433c601ba9f3a41ccc139b160c1616
gregrossdev <gregross.dev@gmail.com>
2022-03-25 14:12:01 -0400

    dynamic accordion feature for photos

--- pages/media/photos.js
+++ pages/media/photos.js
@@ -8,33 +8,30 @@ import Gallery from "../../components/gallery";
 // styles
 import styles from "../../styles/page-media.module.css";
 
-const Photos = ({ photos }) => {
-  
+const Photos = ({ allPhotos, allFolders }) => {
+  const [photoItems, setPhotoItems] = useState(allPhotos);
+  const [folders, setFolders] = useState(allFolders);
+
   const [numOfItems, setNumOfItems] = useState(4);
   const [loading, setLoading] = useState(true);
-  const photoData = JSON.parse(photos);
-
   const [showInfo, setShowInfo] = useState(false);
 
-  const allofThese = (keyword) => {
-    return photoData.filter((i) => {
-      const album = i.Key.split("/")[0];
-      if (album.includes(keyword)) {
-        return i;
-      }
+  const filterItems = (folder, index) => {
+    const updatedItems = allPhotos.filter((file) => {
+      return file.Key.split("/")[0] === folder;
     });
+    setPhotoItems(updatedItems);
+
+    if (showInfo === index) {
+      return setShowInfo(null);
+    }
+
+    setShowInfo(index);
   };
 
   function handleClick() {
     setNumOfItems((prev) => prev + 1);
   }
-
-  const summer = allofThese("summer");
-  const fall = allofThese("fall");
-  const rad = allofThese("radhastami");
-  const cleaning = allofThese("cleaning");
-  const varsana = allofThese("varsana");
-  const devi = allofThese("vrinda");
 
   return (
     <div className="page-container">
@@ -43,69 +40,35 @@ const Photos = ({ photos }) => {
       </header>
 
       <section className="page-section">
-        
-        <div className="accordion">
-          <header className={styles.header}>
-            <h3 className={styles.folder}>One Summer on Govardhan</h3>
-            <button onClick={() => setShowInfo(!showInfo)}>
-              {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
-            </button>
-          </header>
-          {showInfo && (
-            <>
-              <div className="grid">
-                {summer.slice(0, numOfItems).map((item) => {
-                  return <Gallery gallery={item.Key} />;
-                })}
-              </div>
-              <button onClick={handleClick} className="gallery-btn">
-                Load More
-              </button>
-            </>
-          )}
-        </div>
-        
-        <div className="accordion">
-          <header className={styles.header}>
-            <h3 className={styles.folder}>Fall on Govardhan</h3>
-            <button onClick={() => setShowInfo(!showInfo)}>
-              {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
-            </button>
-          </header>
-          {showInfo && (
-            <>
-              <div className="grid">
-                {fall.slice(0, numOfItems).map((item) => {
-                  return <Gallery gallery={item.Key} />;
-                })}
-              </div>
-              <button onClick={handleClick} className="gallery-btn">
-                Load More
-              </button>
-            </>
-          )}
-        </div>
+        {folders.map((category, index) => {
+          return (
+            <div className="accordion" key={index}>
+              <header className={styles.header}>
+                <h3 className={styles.folder}>{category}</h3>
+                <button
+                  type="button"
+                  className="filter-btn"
+                  onClick={() => filterItems(category, index)}
+                >
+                  {showInfo === index ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                </button>
+              </header>
+              {showInfo === index && (
+                <>
+                <div className="grid">
+                  {photoItems.slice(0, numOfItems).map((item) => {
+                    return <Gallery gallery={item.Key} />;
+                  })}
+                </div>
+                <button onClick={handleClick} className="gallery-btn">
+                  Load More
+                </button>
+              </>
+              )}
+            </div>
+          );
+        })}
 
-        {/* <div>
-            
-            <Gallery gallery={items} />
-          </div> */}
-        {/* <div>
-            <h3>Radhastami 2017</h3>
-            <Gallery gallery={photos} />
-          </div>
-          <div>
-            <h3>Cleaning Syama Kunda</h3>
-            <Gallery gallery={photos} />
-          </div>
-          <div>
-            <h3>Varsana Maharaj and Jananivas Prabhu</h3>
-            <Gallery gallery={photos} />
-          </div>
-          <div>
-            <h3>Vrinda-Devi and her birthday cake</h3>
-            <Gallery gallery={photos} />
-          </div> */}
       </section>
     </div>
   );
@@ -116,10 +79,14 @@ export default Photos;
 export async function getStaticProps() {
   const bucketParams = { Bucket: "varsana-photos" };
   const data = await s3Client.send(new ListObjectsCommand(bucketParams));
+  const stringify = JSON.stringify(data.Contents);
+  const allPhotos = JSON.parse(stringify);
+  const allFolders = [...new Set(allPhotos.map((i) => i.Key.split("/")[0]))];
 
   return {
     props: {
-      photos: JSON.stringify(data.Contents),
+      allPhotos,
+      allFolders,
     },
   };
 }

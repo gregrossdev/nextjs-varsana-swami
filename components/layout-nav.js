14ae5c7dd971b80cbcc52a4d0b0e889e0fd1c447
Greg Ross <gregross.dev@gmail.com>
2022-06-18 13:17:12 -0400

    bootstrap fix again for donate button

--- components/layout-nav.js
+++ components/layout-nav.js
@@ -9,7 +9,7 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <Navbar expand="lg" collapseOnSelect>
-        <Container >
+        <Container>
           <Navbar.Brand>
             <Link href="/">
               <a className={styles.logo}>
@@ -29,60 +29,102 @@ export default function Header() {
               <Nav.Link eventKey="1" className={styles.menuItems}>
                 <Link href="/dhamseva">Dham-seva</Link>
               </Nav.Link>
-              <NavDropdown title="Writings" id="basic-nav-dropdown" className={styles.menuItems}>
+              <NavDropdown
+                title="Writings"
+                id="basic-nav-dropdown"
+                className={styles.menuItems}
+              >
                 <NavDropdown.Item eventKey="2">
-                  <Link href="earth-culture/sacramental-nature-of-agriculture-and-plow">Earth Culture</Link>
+                  <Link href="earth-culture/sacramental-nature-of-agriculture-and-plow">
+                    Earth Culture
+                  </Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item eventKey="2">
                   <Link href="/festivals/rope-meditation">Festivals</Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item eventKey="2">
-                  <Link href="/going-deeper/relationship-with-self-and-lord-s-measuring-stick">Going Deeper</Link>
+                  <Link href="/going-deeper/relationship-with-self-and-lord-s-measuring-stick">
+                    Going Deeper
+                  </Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item eventKey="2">
-                  <Link href="/gaura-katha/lord-caitanya-s-taking-sanyas-and-mother-saci">Gaura Katha</Link>
+                  <Link href="/gaura-katha/lord-caitanya-s-taking-sanyas-and-mother-saci">
+                    Gaura Katha
+                  </Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item eventKey="2">
-                  <Link href="/nature-meditations/light-of-the-bhagavat-transcendental-lessons-from-the-natural-world">Nature Meditations</Link>
+                  <Link href="/nature-meditations/light-of-the-bhagavat-transcendental-lessons-from-the-natural-world">
+                    Nature Meditations
+                  </Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item eventKey="2">
-                  <Link href="/personalities/the-story-of-bhakti-devi">Personalities</Link>
+                  <Link href="/personalities/the-story-of-bhakti-devi">
+                    Personalities
+                  </Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item eventKey="2">
-                  <Link href="/srila/i-will-never-die-i-will-live-forever-in-my-books.">Srila Prabhupada Katha</Link>
+                  <Link href="/srila/i-will-never-die-i-will-live-forever-in-my-books.">
+                    Srila Prabhupada Katha
+                  </Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item eventKey="2">
-                  <Link href="/sadhu-sanga/acaryas-their-grace-their-samadhis-and-our-hope">Sadhu-Sanga</Link>
+                  <Link href="/sadhu-sanga/acaryas-their-grace-their-samadhis-and-our-hope">
+                    Sadhu-Sanga
+                  </Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item eventKey="2">
-                  <Link href="/sri-dhama/survey-on-govardhan-hill-in-new-vrindaban">Sri Dhama</Link>
+                  <Link href="/sri-dhama/survey-on-govardhan-hill-in-new-vrindaban">
+                    Sri Dhama
+                  </Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item eventKey="2">
-                  <Link className="white-space" href="/vyasa-puja/vyasa-puja-offering-2021">
-                    Vyasa Puja Offerings 
+                  <Link
+                    className="white-space"
+                    href="/vyasa-puja/vyasa-puja-offering-2021"
+                  >
+                    Vyasa Puja Offerings
                   </Link>
                 </NavDropdown.Item>
                 <NavDropdown.Item eventKey="2">
                   <Link href="/various/japa-sankirtan-and-arcana">Various</Link>
                 </NavDropdown.Item>
               </NavDropdown>
-              <NavDropdown title="Media" id="basic-nav-dropdown" className={styles.menuItems}>
-                <NavDropdown.Item href="https://drive.google.com/drive/folders/1eaTfCEOev0sM1D00Ba_wACKWrxn5p_LG?usp=sharing" target="_blank" rel="noopener noreferrer">
+              <NavDropdown
+                title="Media"
+                id="basic-nav-dropdown"
+                className={styles.menuItems}
+              >
+                <NavDropdown.Item
+                  href="https://drive.google.com/drive/folders/1eaTfCEOev0sM1D00Ba_wACKWrxn5p_LG?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   Audio
                 </NavDropdown.Item>
                 <NavDropdown.Item eventKey="2">
                   <Link href="/media/video">Video</Link>
                 </NavDropdown.Item>
-                <NavDropdown.Item href="https://drive.google.com/drive/folders/1ABHgs1Sesk4ZHUFDhCTRqYhAYzE7HhRr?usp=sharing" target="_blank" rel="noopener noreferrer">
+                <NavDropdown.Item
+                  href="https://drive.google.com/drive/folders/1ABHgs1Sesk4ZHUFDhCTRqYhAYzE7HhRr?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   Photos
                 </NavDropdown.Item>
               </NavDropdown>
               <Nav.Link eventKey="1" className={styles.menuItems}>
                 <Link href="/books">Books</Link>
               </Nav.Link>
-              <a className={`${styles.menuItems} ${styles.donate}`} href="https://www.paypal.com/donate/?hosted_button_id=NQTGB6KQUGFJC" target="_blank" rel="noopener noreferrer">
-                Donate
-              </a>
+              <div>
+                <a
+                  className={`${styles.menuItems} ${styles.donate}`}
+                  href="https://www.paypal.com/donate/?hosted_button_id=NQTGB6KQUGFJC"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Donate
+                </a>
+              </div>
             </Nav>
           </Navbar.Collapse>
         </Container>
